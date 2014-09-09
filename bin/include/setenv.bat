607e4cd4210fdf0689d2c47ca2936da592510814
Valentin Kulichenko <vkulichenko@gridgain.com>
2014-09-08 23:01:54 -0700

    Minor fix.

--- bin/include/setenv.bat
+++ bin/include/setenv.bat
@@ -1,5 +1,18 @@
 ::
-:: @bat.file.header
+:: Copyright (C) GridGain Systems. All Rights Reserved.
+::
+:: Licensed under the Apache License, Version 2.0 (the "License");
+:: you may not use this file except in compliance with the License.
+:: You may obtain a copy of the License at
+
+::    http://www.apache.org/licenses/LICENSE-2.0
+:: 
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:: limitations under the License.
+
 :: _________        _____ __________________        _____
 :: __  ____/___________(_)______  /__  ____/______ ____(_)_______
 :: _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
@@ -41,7 +54,7 @@ if exist %GRIDGAIN_HOME%\libs\gridgain-hadoop* set HADOOP_EDITION=1
 
 if defined USER_LIBS set GRIDGAIN_LIBS=%USER_LIBS%;%GRIDGAIN_LIBS%
 
-if "%HADOOP_EDITION%" == "1" call "%GRIDGAIN_HOME%\os\bin\include\hadoop-classpath.bat"
+if "%HADOOP_EDITION%" == "1" call "%GRIDGAIN_HOME%\bin\include\hadoop-classpath.bat"
 
 set COMMON_HOME_LIB=%HADOOP_COMMON_HOME%\lib
 
--- bin/include/target-classpath.bat
+++ bin/include/target-classpath.bat
@@ -1,5 +1,18 @@
 ::
-:: @bat.file.header
+:: Copyright (C) GridGain Systems. All Rights Reserved.
+::
+:: Licensed under the Apache License, Version 2.0 (the "License");
+:: you may not use this file except in compliance with the License.
+:: You may obtain a copy of the License at
+
+::    http://www.apache.org/licenses/LICENSE-2.0
+:: 
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:: limitations under the License.
+
 :: _________        _____ __________________        _____
 :: __  ____/___________(_)______  /__  ____/______ ____(_)_______
 :: _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
@@ -12,14 +25,14 @@
 :: Target class path resolver.
 ::
 :: Can be used like:
-::       call "%GRIDGAIN_HOME%\os\bin\include\target-classpath.bat"
+::       
 :: in other scripts to set classpath using libs from target folder.
 ::
 :: Will be excluded in release.
 
 @echo off
 
-for /D %%F in (%GRIDGAIN_HOME%\os\modules\*) do if not %%F == "%GRIDGAIN_HOME%\os\modules" call :includeToClassPath %%F
+for /D %%F in (%GRIDGAIN_HOME%\modules\*) do if not %%F == "%GRIDGAIN_HOME%\os\modules" call :includeToClassPath %%F
 
 for /D %%F in (%GRIDGAIN_HOME%\modules\*) do if not %%F == "%GRIDGAIN_HOME%\modules" call :includeToClassPath %%F
 
