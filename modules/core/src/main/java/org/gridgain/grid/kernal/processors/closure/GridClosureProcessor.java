629f4078d025ec32a086ba3f2a4ccb1cf9fc6869
anovikov <anovikov@gridgain.com>
2015-01-23 14:12:29 +0700

    # GG-9711 Compatibility fix.

--- modules/core/src/main/java/org/gridgain/grid/kernal/processors/closure/GridClosureProcessor.java
+++ modules/core/src/main/java/org/gridgain/grid/kernal/processors/closure/GridClosureProcessor.java
@@ -1087,7 +1087,7 @@ public class GridClosureProcessor extends GridProcessorAdapter {
      * @return Grid job made out of closure.
      */
     @SuppressWarnings("IfMayBeConditional")
-    private static GridComputeJob job(final Runnable r) {
+    private GridComputeJob job(final Runnable r) {
         A.notNull(r, "job");
 
         if (r instanceof GridComputeJobMasterLeaveAware) {
