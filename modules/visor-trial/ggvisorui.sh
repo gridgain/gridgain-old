84b1a2f725c59b1f98b0ae5972e12ef5207dd9e7
TeamCity <teamcity@gridgain.com>
2015-04-03 13:55:33 +0300

    Minor fix.

--- modules/visor-trial/ggvisorui.sh
+++ modules/visor-trial/ggvisorui.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 #
-# @sh.file.header
+# Copyright (C) GridGain Systems. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #  _________        _____ __________________        _____
 #  __  ____/___________(_)______  /__  ____/______ ____(_)_______
 #  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
@@ -28,7 +41,7 @@ fi
 #
 # Set SCRIPTS_HOME - base path to scripts.
 #
-SCRIPTS_HOME="${GRIDGAIN_HOME_TMP}/os/bin" # Will be replaced by SCRIPTS_HOME=${GRIDGAIN_HOME_TMP}/bin in release.
+SCRIPTS_HOME="${GRIDGAIN_HOME_TMP}/bin"
 
 source "${SCRIPTS_HOME}"/include/functions.sh
 
@@ -51,7 +64,7 @@ setGridGainHome
 # Set GRIDGAIN_LIBS.
 #
 . "${SCRIPTS_HOME}"/include/setenv.sh
-. "${SCRIPTS_HOME}"/include/target-classpath.sh # Will be removed in release.
+
 CP="${GRIDGAIN_HOME}/bin/include/visor-common/*${SEP}${GRIDGAIN_HOME}/bin/include/visorui/*${SEP}${GRIDGAIN_LIBS}"
 
 #
