24e50ad51e6af48fffc2de25e033f489c83c24f8
sboikov <sboikov@gridgain.com>
2015-04-28 11:05:44 +0300

    gg-6.6.5 Backported test for near tx rollback.

---
+++ modules/core/src/test/java/org/gridgain/grid/kernal/processors/cache/distributed/near/GridCacheNearTxRollbackTest.java
@@ -0,0 +1,136 @@
+/* @java.file.header */
+
+/*  _________        _____ __________________        _____
+ *  __  ____/___________(_)______  /__  ____/______ ____(_)_______
+ *  _  / __  __  ___/__  / _  __  / _  / __  _  __ `/__  / __  __ \
+ *  / /_/ /  _  /    _  /  / /_/ /  / /_/ /  / /_/ / _  /  _  / / /
+ *  \____/   /_/     /_/   \_,__/   \____/   \__,_/  /_/   /_/ /_/
+ */
+
+package org.gridgain.grid.kernal.processors.cache.distributed.near;
+
+import org.gridgain.grid.*;
+import org.gridgain.grid.cache.*;
+import org.gridgain.grid.kernal.managers.communication.*;
+import org.gridgain.grid.kernal.processors.cache.*;
+import org.gridgain.grid.spi.*;
+import org.gridgain.grid.spi.communication.tcp.*;
+import org.gridgain.grid.util.direct.*;
+import org.gridgain.grid.util.typedef.*;
+
+import java.util.*;
+
+import static org.gridgain.grid.cache.GridCacheAtomicityMode.*;
+import static org.gridgain.grid.cache.GridCacheDistributionMode.*;
+import static org.gridgain.grid.cache.GridCacheMode.*;
+import static org.gridgain.grid.cache.GridCacheTxConcurrency.*;
+import static org.gridgain.grid.cache.GridCacheTxIsolation.*;
+
+/**
+ *
+ */
+public class GridCacheNearTxRollbackTest extends GridCacheAbstractSelfTest {
+    /** {@inheritDoc} */
+    @Override protected int gridCount() {
+        return 3;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected GridCacheMode cacheMode() {
+        return PARTITIONED;
+    }
+
+    /** {@inheritDoc} */
+    @SuppressWarnings("RedundantMethodOverride")
+    @Override protected GridCacheAtomicityMode atomicityMode() {
+        return TRANSACTIONAL;
+    }
+
+    /** {@inheritDoc} */
+    @SuppressWarnings("RedundantMethodOverride")
+    @Override protected GridCacheDistributionMode distributionMode() {
+        return NEAR_PARTITIONED;
+    }
+
+    /** {@inheritDoc} */
+    @Override protected GridConfiguration getConfiguration(String gridName) throws Exception {
+        GridConfiguration cfg = super.getConfiguration(gridName);
+
+        cfg.setCommunicationSpi(new TestCommunicationSpi());
+
+        return cfg;
+    }
+
+    /**
+     * @throws Exception If failed.
+     */
+    public void testPutAllRollback() throws Exception {
+        GridCache<Integer, Integer> cache = grid(0).cache(null);
+
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+
+        map.put(nearKey(cache), 1);
+        map.put(primaryKey(cache), 1);
+
+        TestCommunicationSpi spi = (TestCommunicationSpi)grid(0).configuration().getCommunicationSpi();
+
+        spi.sndFail = true;
+
+        try {
+            try {
+                try (GridCacheTx tx = cache.txStart(OPTIMISTIC, REPEATABLE_READ)) {
+                    cache.putAll(map);
+
+                    tx.commit();
+                }
+
+                fail("Put should fail.");
+            }
+            catch (GridException e) {
+                log.info("Expected exception: " + e);
+
+                assertFalse(X.hasCause(e, AssertionError.class));
+            }
+
+            for (int i = 0; i < gridCount(); i++) {
+                for (Integer key : map.keySet()) {
+                    GridCache<Integer, Integer> cache0 = grid(i).cache(null);
+
+                    assertNull(cache0.peek(key));
+                }
+            }
+
+            spi.sndFail = false;
+
+            cache.putAll(map);
+
+            for (Map.Entry<Integer, Integer> e : map.entrySet())
+                assertEquals(e.getValue(), cache.get(e.getKey()));
+        }
+        finally {
+            spi.sndFail = false;
+        }
+    }
+
+    /**
+     *
+     */
+    private static class TestCommunicationSpi extends GridTcpCommunicationSpi {
+        /** */
+        private volatile boolean sndFail;
+
+        /** {@inheritDoc} */
+        @Override public void sendMessage(GridNode node, GridTcpCommunicationMessageAdapter msg)
+            throws GridSpiException
+        {
+            if (msg instanceof GridIoMessage) {
+                Object msg0 = ((GridIoMessage)msg).message();
+
+                if (sndFail && msg0 instanceof GridNearTxPrepareRequest)
+                    throw new GridSpiException("Test error");
+            }
+
+            super.sendMessage(node, msg);
+        }
+    }
+}
